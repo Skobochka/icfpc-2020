053e759346afa9b5a3dfed28b699180ca66aa9da
Gleb Golubitsky <sectoid@gnolltech.org>
2020-07-16 01:53:13 +0300

    [Test] Fix exit code handling for SMOKE test
    This commit should FAIL the CI build
    
    Signed-off-by: Gleb Golubitsky <sectoid@gnolltech.org>

--- run.sh
+++ run.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 
-/solution/target/release/app "$@" || echo "run error code: $?"
+/solution/target/release/app "$@"
+
+RETVAL=$?
+
+if [ $RETVAL -ne 0 ]
+then
+  echo "run error code: $RETVAL"
+  exit $RETVAL
+else
+  exit 0
+fi

0314c4fd0423f659aaee487bf0b0c0daa5c66373
Alexey Voznyuk <me@swizard.info>
2020-07-18 01:25:25 +0300

    first op interpreted: `inc`

--- common/src/vm/interpret.rs
+++ common/src/vm/interpret.rs
@@ -2,6 +2,11 @@ use super::{
     super::code::{
         Op,
         Ops,
+        Fun,
+        Const,
+        Number,
+        EncodedNumber,
+        PositiveNumber,
     },
     Env,
 };
@@ -14,6 +19,8 @@ pub struct Interpreter {
 pub enum Error {
     NoAppFunProvided,
     NoAppArgProvided { fun: AstNode, },
+    EvalEmptyTree,
+    AppOnNumber { number: EncodedNumber, arg: AstNode, },
 }
 
 #[derive(Clone, PartialEq, Eq, Hash, Debug)]
@@ -35,7 +42,7 @@ impl Interpreter {
         }
     }
 
-    pub fn build_tree(&self, Ops(ops): Ops, _env: &mut Env) -> Result<Ast, Error> {
+    pub fn build_tree(&self, Ops(ops): Ops) -> Result<Ast, Error> {
         enum State {
             AwaitAppFun,
             AwaitAppArg { fun: AstNode, },
@@ -80,6 +87,165 @@ impl Interpreter {
             }
         }
     }
+
+    pub fn eval(&self, ast: Ast, env: &mut Env) -> Result<Ops, Error> {
+        match ast {
+            Ast::Empty =>
+                Err(Error::EvalEmptyTree),
+            Ast::Tree(node) =>
+                self.eval_tree(node, env),
+        }
+    }
+
+    fn eval_tree(&self, mut ast_node: AstNode, _env: &mut Env) -> Result<Ops, Error> {
+        enum State {
+            EvalAppFun { arg: AstNode, },
+            EvalAppArg { fun: EvalFun, },
+        }
+
+        let mut states = vec![];
+        loop {
+            let mut eval_op = match ast_node {
+                AstNode::Literal { value, } =>
+                    EvalOp::new(value),
+
+                AstNode::App { fun, arg, } => {
+                    states.push(State::EvalAppFun { arg: *arg, });
+                    ast_node = *fun;
+                    continue;
+                },
+            };
+
+            loop {
+                match (states.pop(), eval_op) {
+                    (None, EvalOp::Number { number, }) =>
+                        return Ok(Ops(vec![Op::Const(Const::EncodedNumber(number))])),
+                    (None, EvalOp::Fun(EvalFun::Inc0)) =>
+                        return Ok(Ops(vec![Op::Const(Const::Fun(Fun::Inc))])),
+                    (None, EvalOp::Fun(EvalFun::Dec0)) =>
+                        return Ok(Ops(vec![Op::Const(Const::Fun(Fun::Dec))])),
+
+                    (Some(State::EvalAppFun { arg, }), EvalOp::Number { number, }) =>
+                        return Err(Error::AppOnNumber { number, arg, }),
+                    (Some(State::EvalAppFun { arg, }), EvalOp::Fun(fun)) => {
+                        states.push(State::EvalAppArg { fun, });
+                        ast_node = arg;
+                        break;
+                    },
+
+                    (
+                        Some(State::EvalAppArg { fun: EvalFun::Inc0, }),
+                        EvalOp::Number {
+                            number: EncodedNumber {
+                                number: Number::Positive(PositiveNumber { value, }),
+                                modulation,
+                            },
+                        },
+                    ) =>
+                        eval_op = EvalOp::Number {
+                            number: EncodedNumber {
+                                number: Number::Positive(PositiveNumber { value: value + 1, }),
+                                modulation,
+                            },
+                        },
+
+                    _ =>
+                        unimplemented!(),
+                }
+            }
+        }
+    }
+}
+
+enum EvalOp {
+    Number { number: EncodedNumber, },
+    Fun(EvalFun),
+}
+
+enum EvalFun {
+    Inc0,
+    Dec0,
+}
+
+impl EvalOp {
+    fn new(op: Op) -> EvalOp {
+        match op {
+            Op::Const(Const::EncodedNumber(number)) =>
+                EvalOp::Number { number, },
+            Op::Const(Const::Fun(Fun::Inc)) =>
+                EvalOp::Fun(EvalFun::Inc0),
+            Op::Const(Const::Fun(Fun::Dec)) =>
+                EvalOp::Fun(EvalFun::Dec0),
+            Op::Const(Const::Fun(Fun::Sum)) =>
+                unimplemented!(),
+            Op::Const(Const::Fun(Fun::Mul)) =>
+                unimplemented!(),
+            Op::Const(Const::Fun(Fun::Div)) =>
+                unimplemented!(),
+            Op::Const(Const::Fun(Fun::Eq)) =>
+                unimplemented!(),
+            Op::Const(Const::Fun(Fun::Lt)) =>
+                unimplemented!(),
+            Op::Const(Const::Fun(Fun::Mod)) =>
+                unimplemented!(),
+            Op::Const(Const::Fun(Fun::Dem)) =>
+                unimplemented!(),
+            Op::Const(Const::Fun(Fun::Send)) =>
+                unimplemented!(),
+            Op::Const(Const::Fun(Fun::Neg)) =>
+                unimplemented!(),
+            Op::Const(Const::Fun(Fun::S)) =>
+                unimplemented!(),
+            Op::Const(Const::Fun(Fun::C)) =>
+                unimplemented!(),
+            Op::Const(Const::Fun(Fun::B)) =>
+                unimplemented!(),
+            Op::Const(Const::Fun(Fun::True)) =>
+                unimplemented!(),
+            Op::Const(Const::Fun(Fun::False)) =>
+                unimplemented!(),
+            Op::Const(Const::Fun(Fun::Pwr2)) =>
+                unimplemented!(),
+            Op::Const(Const::Fun(Fun::I)) =>
+                unimplemented!(),
+            Op::Const(Const::Fun(Fun::Cons)) =>
+                unimplemented!(),
+            Op::Const(Const::Fun(Fun::Car)) =>
+                unimplemented!(),
+            Op::Const(Const::Fun(Fun::Cdr)) =>
+                unimplemented!(),
+            Op::Const(Const::Fun(Fun::Nil)) =>
+                unimplemented!(),
+            Op::Const(Const::Fun(Fun::IsNil)) =>
+                unimplemented!(),
+            Op::Const(Const::Fun(Fun::LeftParen)) =>
+                unimplemented!(),
+            Op::Const(Const::Fun(Fun::Comma)) =>
+                unimplemented!(),
+            Op::Const(Const::Fun(Fun::RightParen)) =>
+                unimplemented!(),
+            Op::Const(Const::Fun(Fun::Vec)) =>
+                unimplemented!(),
+            Op::Const(Const::Fun(Fun::Draw)) =>
+                unimplemented!(),
+            Op::Const(Const::Fun(Fun::Chkb)) =>
+                unimplemented!(),
+            Op::Const(Const::Fun(Fun::MultipleDraw)) =>
+                unimplemented!(),
+            Op::Const(Const::Fun(Fun::If0)) =>
+                unimplemented!(),
+            Op::Const(Const::Fun(Fun::Interact)) =>
+                unimplemented!(),
+            Op::Const(Const::Fun(Fun::Modem)) =>
+                unimplemented!(),
+            Op::Const(Const::Fun(Fun::Galaxy)) =>
+                unimplemented!(),
+            Op::Variable(var) =>
+                unimplemented!(),
+            Op::App =>
+                unreachable!(),
+        }
+    }
 }
 
 #[cfg(test)]
@@ -106,13 +272,12 @@ mod tests {
     };
 
     #[test]
-    fn interpret_basic() {
+    fn ast_tree_basic() {
         let interpreter = Interpreter::new();
 
         assert_eq!(
             interpreter.build_tree(
                 Ops(vec![]),
-                &mut Env::new(),
             ),
             Ok(Ast::Empty),
         );
@@ -127,7 +292,6 @@ mod tests {
                         modulation: Modulation::Demodulated,
                     })),
                 ]),
-                &mut Env::new(),
             ),
             Ok(Ast::Tree(AstNode::Literal {
                 value: Op::Const(Const::EncodedNumber(EncodedNumber {
@@ -142,7 +306,6 @@ mod tests {
         assert_eq!(
             interpreter.build_tree(
                 Ops(vec![Op::App]),
-                &mut Env::new(),
             ),
             Err(Error::NoAppFunProvided),
         );
@@ -150,7 +313,6 @@ mod tests {
         assert_eq!(
             interpreter.build_tree(
                 Ops(vec![Op::App, Op::App]),
-                &mut Env::new(),
             ),
             Err(Error::NoAppFunProvided),
         );
@@ -158,7 +320,6 @@ mod tests {
         assert_eq!(
             interpreter.build_tree(
                 Ops(vec![Op::App, Op::Const(Const::Fun(Fun::Inc))]),
-                &mut Env::new(),
             ),
             Err(Error::NoAppArgProvided { fun: AstNode::Literal { value: Op::Const(Const::Fun(Fun::Inc)), }, }),
         );
@@ -179,7 +340,6 @@ mod tests {
                         modulation: Modulation::Demodulated,
                     })),
                 ]),
-                &mut Env::new(),
             ),
             Ok(Ast::Tree(AstNode::App {
                 fun: Box::new(AstNode::Literal {
@@ -222,7 +382,6 @@ mod tests {
                         modulation: Modulation::Demodulated,
                     })),
                 ]),
-                &mut Env::new(),
             ),
             Ok(Ast::Tree(AstNode::App {
                 fun: Box::new(AstNode::App {
@@ -251,5 +410,113 @@ mod tests {
                 }),
             })),
         );
+    }
+
+    #[test]
+    fn eval_basic() {
+        let interpreter = Interpreter::new();
+
+        assert_eq!(
+            interpreter.eval(
+                interpreter.build_tree(
+                    Ops(vec![]),
+                ).unwrap(),
+                &mut Env::new(),
+            ),
+            Err(Error::EvalEmptyTree),
+        );
+
+        assert_eq!(
+            interpreter.eval(
+                interpreter.build_tree(
+                    Ops(vec![
+                        Op::Const(Const::EncodedNumber(EncodedNumber {
+                            number: Number::Positive(PositiveNumber {
+                                value: 1,
+                            }),
+                            modulation: Modulation::Demodulated,
+                        })),
+                    ]),
+                ).unwrap(),
+                &mut Env::new(),
+            ),
+            Ok(Ops(vec![
+                Op::Const(Const::EncodedNumber(EncodedNumber {
+                    number: Number::Positive(PositiveNumber {
+                        value: 1,
+                    }),
+                    modulation: Modulation::Demodulated,
+                })),
+            ])),
+        );
+
+    }
+
+    #[test]
+    fn eval_fun_inc() {
+        let interpreter = Interpreter::new();
+        assert_eq!(
+            interpreter.eval(
+                interpreter.build_tree(
+                    Ops(vec![
+                        Op::App,
+                        Op::Const(Const::EncodedNumber(EncodedNumber {
+                            number: Number::Positive(PositiveNumber {
+                                value: 1,
+                            }),
+                            modulation: Modulation::Demodulated,
+                        })),
+                        Op::Variable(Variable {
+                            name: Number::Positive(PositiveNumber {
+                                value: 1,
+                            }),
+                        }),
+                    ]),
+                ).unwrap(),
+                &mut Env::new(),
+            ),
+            Err(Error::AppOnNumber {
+                number: EncodedNumber {
+                    number: Number::Positive(PositiveNumber {
+                        value: 1,
+                    }),
+                    modulation: Modulation::Demodulated,
+                },
+                arg: AstNode::Literal {
+                    value: Op::Variable(Variable {
+                        name: Number::Positive(PositiveNumber {
+                            value: 1,
+                        }),
+                    }),
+                },
+            }),
+        );
+
+        assert_eq!(
+            interpreter.eval(
+                interpreter.build_tree(
+                    Ops(vec![
+                        Op::App,
+                        Op::Const(Const::Fun(Fun::Inc)),
+                        Op::Const(Const::EncodedNumber(EncodedNumber {
+                            number: Number::Positive(PositiveNumber {
+                                value: 1,
+                            }),
+                            modulation: Modulation::Demodulated,
+                        })),
+                    ]),
+                ).unwrap(),
+                &mut Env::new(),
+            ),
+            Ok(Ops(vec![
+                Op::Const(Const::EncodedNumber(EncodedNumber {
+                    number: Number::Positive(PositiveNumber {
+                        value: 2,
+                    }),
+                    modulation: Modulation::Demodulated,
+                })),
+            ])),
+        );
+
     }
 }

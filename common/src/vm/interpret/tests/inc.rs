3b1fede44e65f6e561e171454bbae98dbf1bc883
Alexey Voznyuk <me@swizard.info>
2020-07-18 03:20:17 +0300

    long testcase

--- common/src/vm/interpret/tests/inc.rs
+++ common/src/vm/interpret/tests/inc.rs
@@ -9,6 +9,7 @@ use super::{
     Fun,
     Const,
     Number,
+    Variable,
     Modulation,
     EncodedNumber,
     PositiveNumber,
@@ -110,4 +111,22 @@ fn eval() {
         ),
         Err(Error::AppExpectsNumButFunProvided { fun: EvalFun::ArgNum(EvalFunNum::Inc0), }),
     );
+
+    // assert_eq!(
+    //     interpreter.eval(
+    //         interpreter.build_tree(
+    //             Ops(vec![
+    //                 Op::App,
+    //                 Op::Const(Const::Fun(Fun::Inc)),
+    //                 Op::Variable(Variable {
+    //                     name: Number::Positive(PositiveNumber {
+    //                         value: 1,
+    //                     }),
+    //                 }),
+    //             ]),
+    //         ).unwrap(),
+    //         &mut Env::new(),
+    //     ),
+    //     Ok(Ops(vec![])),
+    // );
 }
--- common/src/vm/interpret/tests/sum.rs
+++ common/src/vm/interpret/tests/sum.rs
@@ -51,4 +51,85 @@ fn eval() {
             })),
         ])),
     );
+
+    assert_eq!(
+        interpreter.eval(
+            interpreter.build_tree(
+                Ops(vec![
+                    Op::App,
+                    Op::App,
+                    Op::Const(Const::Fun(Fun::Sum)),
+                    Op::Const(Const::EncodedNumber(EncodedNumber {
+                        number: Number::Positive(PositiveNumber {
+                            value: 1,
+                        }),
+                        modulation: Modulation::Modulated,
+                    })),
+                    Op::Const(Const::EncodedNumber(EncodedNumber {
+                        number: Number::Positive(PositiveNumber {
+                            value: 3,
+                        }),
+                        modulation: Modulation::Demodulated,
+                    })),
+                ]),
+            ).unwrap(),
+            &mut Env::new(),
+        ),
+        Err(Error::AddTwoNumbersInDifferentModulation {
+            number_a: EncodedNumber {
+                number: Number::Positive(PositiveNumber { value: 1, }), modulation: Modulation::Modulated,
+            },
+            number_b: EncodedNumber {
+                number: Number::Positive(PositiveNumber { value: 3, }), modulation: Modulation::Demodulated,
+            },
+        }),
+    );
+}
+
+#[test]
+fn eval_long() {
+    let interpreter = Interpreter::new();
+
+    // ap ap add ap ap add 2 3 4 = 9
+    assert_eq!(
+        interpreter.eval(
+            interpreter.build_tree(
+                Ops(vec![
+                    Op::App,
+                    Op::App,
+                    Op::Const(Const::Fun(Fun::Sum)),
+                    Op::App,
+                    Op::App,
+                    Op::Const(Const::Fun(Fun::Sum)),
+                    Op::Const(Const::EncodedNumber(EncodedNumber {
+                        number: Number::Positive(PositiveNumber {
+                            value: 2,
+                        }),
+                        modulation: Modulation::Demodulated,
+                    })),
+                    Op::Const(Const::EncodedNumber(EncodedNumber {
+                        number: Number::Positive(PositiveNumber {
+                            value: 3,
+                        }),
+                        modulation: Modulation::Demodulated,
+                    })),
+                    Op::Const(Const::EncodedNumber(EncodedNumber {
+                        number: Number::Positive(PositiveNumber {
+                            value: 4,
+                        }),
+                        modulation: Modulation::Demodulated,
+                    })),
+                ]),
+            ).unwrap(),
+            &mut Env::new(),
+        ),
+        Ok(Ops(vec![
+            Op::Const(Const::EncodedNumber(EncodedNumber {
+                number: Number::Positive(PositiveNumber {
+                    value: 9,
+                }),
+                modulation: Modulation::Demodulated,
+            })),
+        ])),
+    );
 }

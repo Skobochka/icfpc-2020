0d7ad8c6a2c59b2ba7bf94542ad03ebc73e5ed3a
Gleb Golubitsky <sectoid@gnolltech.org>
2020-07-20 01:30:21 +0300

    game: basic game round structures
    Signed-off-by: Gleb Golubitsky <sectoid@gnolltech.org>

--- common/src/game.rs
+++ common/src/game.rs
@@ -18,39 +18,61 @@ pub enum Role {
 }
 
 #[derive(Clone, PartialEq, Eq, Hash, Debug)]
-pub struct StaticGameInfo {
+pub struct GameStaticInfo {
     // (x0 role x2 x3 x4)
-    x0_raw: String,
-    role: Role,
-    x2_raw: String,
-    x3_raw: String,
-    x4_raw: String,
+    pub x0_raw: String,
+    pub role: Role,
+    pub x2_raw: String,
+    pub x3_raw: String,
+    pub x4_raw: String,
 }
 
-// pub struct ShipsAndCommands {
-//     // TODO:
-// }
 
-// pub struct GameState {
-//     gameTick: usize,
-//     x1: String, //unknown
-//     shipsAndCommands: //
-// }
+#[derive(Clone, PartialEq, Eq, Hash, Debug)]
+pub struct Ship {
+    pub role: Role,
+    pub ship_id: isize,
+    pub position: Vec2,
+    pub velocity: Vec2,
+    pub x4_raw: String,
+    pub x5_raw: String,
+    pub x6_raw: String,
+    pub x7_raw: String,
+}
+
+#[derive(Clone, PartialEq, Eq, Hash, Debug)]
+pub enum Command {
+    Accelerate { ship_id: isize, vec: Vec2 }, // (0, shipId, vector)
+    Detonate { ship_id: isize }, // (1, shipId)
+    Shoot { ship_id: isize, target: Vec2, x3_raw: String }, // (2, shipId, target, x3)
+}
+
+#[derive(Clone, PartialEq, Eq, Hash, Debug)]
+pub struct GameState {
+    pub game_tick: usize,
+    pub x1: String, //unknown
+    pub ships_n_commands: Vec<(Ship, Vec<Command>)>,
+}
 
 #[derive(Clone, PartialEq, Eq, Hash, Debug)]
 pub struct GameResponse {
-    
+    pub stage: GameStage,
+    pub static_info: GameStaticInfo,
+    pub state: GameState,
 }
 
 #[derive(Clone, PartialEq, Eq, Hash, Debug)]
 pub struct GameRound {
     pub player_key: usize,
+    pub last_response: Option<GameResponse>
+
 }
 
 impl GameRound {
     pub fn new(player_key: &str) -> GameRound {
         GameRound {
             player_key: usize::from_str_radix(player_key, 10).unwrap(),
+            last_response: None,
         }
     }
 }

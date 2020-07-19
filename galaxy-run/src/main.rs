fdb041786d94e980bc6b68a1accc3cceafb51749
Alexey Voznyuk <me@swizard.info>
2020-07-19 15:02:46 +0300

    readline for galaxy run

--- galaxy-run/src/main.rs
+++ galaxy-run/src/main.rs
@@ -1,35 +1,65 @@
+use std::io;
 
-use std::io::{
-    BufReader,BufRead,Write,
+use rustyline::{
+    error::ReadlineError,
+    Editor,
 };
 
-use common::proto::{Session,Error};
+use common::proto::{
+    Session,
+};
 
+#[derive(Debug)]
+enum Error {
+    Proto(common::proto::Error),
+    Readline(ReadlineError),
+}
 
-fn main() -> Result<(),Error> {
-    let mut session = Session::galaxy()?;
+fn main() -> Result<(), Error> {
+
+    let mut rl = Editor::<()>::new();
+    match rl.load_history("./galaxy-run-history.txt") {
+        Ok(()) =>
+            (),
+        Err(ReadlineError::Io(ref e)) if e.kind() == io::ErrorKind::NotFound => {
+            println!("no previous history in current dir");
+        },
+        Err(e) =>
+            return Err(Error::Readline(e)),
+    }
+
+    let mut session = Session::galaxy().map_err(Error::Proto)?;
 
     println!("Enter a command to run\nFor example: ap galaxy 0\nOr 'exit' to exit...\n");
-    print!(">>> "); std::io::stdout().flush().ok();
-    for row in BufReader::new(std::io::stdin()).lines() {
-        let asm = match row {
-            Err(e) => {
-                println!("Read error: {:?}\n",e);
-                print!(">>> "); std::io::stdout().flush().ok();
-                continue;
+    loop {
+        let readline = rl.readline(">>> ");
+        let asm = match readline {
+            Ok(line) => {
+                rl.add_history_entry(line.as_str());
+                line
             },
-            Ok(asm) => asm,
+            Err(ReadlineError::Interrupted) => {
+                println!("Exit on <CTRL-C>");
+                break
+            },
+            Err(ReadlineError::Eof) => {
+                println!("Exit on <CTRL-D>");
+                break
+            },
+            Err(err) => {
+                println!("Read rrror: {:?}", err);
+                break
+            }
         };
         match &asm[..] {
             "exit" => {
                 println!("Bye...");
-                return Ok(());
+                break;
             },
-            "" => {
-                print!(">>> "); std::io::stdout().flush().ok();
-                continue;
-            },
-            _ => {},
+            "" =>
+                continue,
+            _ =>
+                (),
         }
         match session.eval_asm(&asm) {
             Ok(ops) => {
@@ -43,7 +73,7 @@ fn main() -> Result<(),Error> {
                 println!("Error: {:?}",e);
             },
         }
-        print!(">>> "); std::io::stdout().flush().ok();
     }
+    rl.save_history("./galaxy-run-history.txt").unwrap();
     Ok(())
 }

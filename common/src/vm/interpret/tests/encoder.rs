dd0510ef96bd76d97978ffa33835ff488bb3c960
Alexey Voznyuk <me@swizard.info>
2020-07-19 18:32:57 +0300

    one more test

--- common/src/vm/interpret/tests/encoder.rs
+++ common/src/vm/interpret/tests/encoder.rs
@@ -40,6 +40,29 @@ fn encode() {
                 Op::Syntax(Syntax::LeftParen),
                 Op::Const(Const::EncodedNumber(EncodedNumber {
                     number: Number::Positive(PositiveNumber {
+                        value: 0,
+                    }),
+                    modulation: Modulation::Demodulated,
+                })),
+                Op::Syntax(Syntax::RightParen),
+            ]),
+            &Env::new(),
+        ).unwrap(),
+        ListVal::Cons(Box::new(ConsList::Cons(
+            ListVal::Number(EncodedNumber {
+                number: Number::Positive(PositiveNumber { value: 0, }),
+                modulation: Modulation::Demodulated,
+            }),
+            ListVal::Cons(Box::new(ConsList::Nil)),
+        ))),
+    );
+
+    assert_eq!(
+        interpreter.eval_ops_to_list_val(
+            Ops(vec![
+                Op::Syntax(Syntax::LeftParen),
+                Op::Const(Const::EncodedNumber(EncodedNumber {
+                    number: Number::Positive(PositiveNumber {
                         value: 1,
                     }),
                     modulation: Modulation::Demodulated,

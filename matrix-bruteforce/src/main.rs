210d3b371e9e04c85bfb8e9e9cc8a20801b17a22
Alexey Voznyuk <me@swizard.info>
2020-07-22 04:39:38 +0300

    multithreaded bruteforcer

--- matrix-bruteforce/src/main.rs
+++ matrix-bruteforce/src/main.rs
@@ -1,3 +1,11 @@
+use std::{
+    sync::{
+        Arc,
+        Mutex,
+    },
+    thread,
+};
+
 use rand::Rng;
 
 use common::{
@@ -34,17 +42,10 @@ enum Error {
 }
 
 fn main() -> Result<(), Error> {
-    let mut session = Session::with_interpreter(
-        galaxy(),
-        Interpreter::new(),
-    ).map_err(Error::Proto)?;
-
     let parser = AsmParser::new();
     let state_asm = "(4, (1, (122, 203, 410, 164, 444, 484, 202, 77, 251, 56, 456, 435, 28, 329, 257, 265, 501, 18, 190, 423, 384, 434, 266, 69, 34, 437, 203, 152, 160, 425, 245, 428, 99, 107, 192, 372, 346, 344, 169, 478, 393, 502, 201, 497, 313, 32, 281, 510, 436, 22, 237, 80, 325, 405, 184, 358, 57, 276, 359, 189, 284, 277, 198, 244), -1, 0, nil), 0, (103652820))";
-    let mut last_good_state_ops = parser.parse_expression(state_asm)
+    let init_state_ops = parser.parse_expression(state_asm)
         .map_err(Error::InitStateParse)?;
-
-    let mut found = Ops(vec![]);
     let points: Vec<_> = (0 .. 8)
         .flat_map(|row| (0 .. 8).map(move |col| (row, col)))
         .collect();
@@ -54,38 +55,61 @@ fn main() -> Result<(), Error> {
             pairs.push((points[i], points[j]));
         }
     }
-    let mut state_ops = last_good_state_ops.clone();
-    let mut rng = rand::thread_rng();
-    while !pairs.is_empty() {
-        let index = rng.gen_range(0, pairs.len());
-        let pair = pairs.swap_remove(index);
+    let sync_pairs = Arc::new(Mutex::new(pairs));
 
-        state_ops = run_on_state_coords(&mut session, state_ops, pair.0)?;
-        state_ops = run_on_state_coords(&mut session, state_ops, pair.1)?;
-        let results = results_list(&mut session, &parser, &state_ops)?;
-        if found.0.is_empty() {
-            found = results.clone();
-        }
-        match (&*results.0, &*found.0) {
-            ([Op::Const(Const::ModulatedBits(results_bits))], [Op::Const(Const::ModulatedBits(found_bits))]) => {
-                if results_bits.len() > found_bits.len() {
-                    println!(
-                        " ;; a new pair is found @ {:?}! {:?}",
-                        pair,
-                        encoder::ConsList::demodulate_from_string(results_bits)
-                            .unwrap()
-                            .to_pretty_string(),
-                    );
-                    found = results;
-                    last_good_state_ops = state_ops.clone();
-                } else {
-                    println!(" ;; rejecting pair {:?}, restoring previous session ({} pairs left)", pair, pairs.len());
-                    state_ops = last_good_state_ops.clone();
+    let mut slaves = vec![];
+    for _ in 0 .. 4 {
+        let sync_pairs = sync_pairs.clone();
+        let init_state_ops = init_state_ops.clone();
+
+        slaves.push(thread::spawn(move || {
+            let mut session = Session::with_interpreter(
+                galaxy(),
+                Interpreter::new(),
+            ).map_err(Error::Proto)?;
+            let parser = AsmParser::new();
+
+            let mut state_ops = init_state_ops.clone();
+
+            let mut rng = rand::thread_rng();
+            loop {
+                let pair = {
+                    let mut pairs = sync_pairs.lock().unwrap();
+                    if pairs.is_empty() {
+                        break;
+                    }
+                    let index = rng.gen_range(0, pairs.len());
+                    pairs.swap_remove(index)
+                };
+
+                state_ops = run_on_state_coords(&mut session, state_ops, pair.0)?;
+                state_ops = run_on_state_coords(&mut session, state_ops, pair.1)?;
+                let results = results_list(&mut session, &parser, &state_ops)?;
+                match &*results.0 {
+                    [Op::Const(Const::ModulatedBits(results_bits))] => {
+                        if results_bits.len() > 2 {
+                            println!(
+                                " ;; a new pair is found @ {:?}! {:?}",
+                                pair,
+                                encoder::ConsList::demodulate_from_string(results_bits)
+                                    .unwrap()
+                                    .to_pretty_string(),
+                            );
+                        } else {
+                            println!(" ;; rejecting pair {:?}, restoring previous session ({} pairs left)", pair, sync_pairs.lock().unwrap().len());
+                            state_ops = init_state_ops.clone();
+                        }
+                    },
+                    _ =>
+                        panic!("unexpected values result = {:?}", results),
                 }
-            },
-            _ =>
-                panic!("unexpected values result = {:?}, found = {:?}", results, found),
-        }
+            }
+            Ok(())
+        }));
+    }
+
+    while let Some(slave) = slaves.pop() {
+        slave.join().unwrap()?;
     }
 
     Ok(())
